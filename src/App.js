9bb906bf60e231eddc60d4167eb3887cf5ce1f5b
Byung Jae Cho <bc454@cornell.edu>
2019-04-30 16:26:36 -0400

    day 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavItem} from "react-bootstrap";
 import Routes from "./Routes";
 import './App.css';
 import { LinkContainer } from "react-router-bootstrap";
 
 
 
+
+
 class App extends Component {
+
   render() {
     return (
       <div className="App container">
-        <Navbar fluid collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">LAMWITTY Data Collection</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-        </Navbar>
         <Routes />
       </div>
     );
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {ButtonToolbar, Button } from "react-bootstrap";
 import "./Home.css";
 
 export default class Home extends Component {
@@ -7,9 +8,24 @@ export default class Home extends Component {
       <div className="Home">
         <div className="lander">
           <h1>Project: Look at Me when I Talk to You</h1>
-          <p> We are trying to collect dataset of people interacting with a voice agent like Amazon Alexa.
-          There is no risk involved with this study but if you agree to participate,
-          you will be video recorded and audio recorded.</p>
+          <h3> We’re building an open video dataset of human interactions with voice agents like Alexa.
+          There are no risks to participating (other than the typical frustration when the chatbot doesn’t respond as you were expecting),
+          but if you agree to participate then you will be asked to play Blackjack against Alexa and your interactions with Alexa
+          will be audio and video-recorded.
+          <ButtonToolbar>
+            <a href= "https://www.youtube.com/watch?v=qd5oc9hLrXg" class="btn btn-primary btn-lg">
+                I don't know how to play blackjack
+            </a>
+          </ButtonToolbar>
+
+          </h3>
+        </div>
+        <div className = "box text-center">
+          <ButtonToolbar>
+            <a href= "/login" class="btn btn-primary btn-lg btn-block">
+              Click here to Start
+            </a>
+          </ButtonToolbar>
         </div>
       </div>
     );
