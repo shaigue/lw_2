a4f37b265775bb4e8ac4ac76687d73415e422812
Byung Jae Cho <bc454@cornell.edu>
2019-05-02 18:45:46 -0400

    video debug

--- src/containers/defaults/loading-view.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styled from 'styled-components'
-
-const LoadingMessage = styled.div`
-  font-family: Arial;
-`
-
-export default () => <LoadingMessage>Loading...</LoadingMessage>
--- src/containers/defaults/unsupported-view.js
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export default () => <div>This browser is uncapable of recording video</div>
